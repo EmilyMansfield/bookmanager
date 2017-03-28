30affb67ae093200dc4a0abbd063e80d6391fd93
Daniel Mansfield <daniel.mansfield99@gmail.com>
2017-03-28 12:02:02 +0100

    Made library loading more robust and reduced scope of argument try/catch

--- src/main/kotlin/com/dbmansfield/book/Args.kt
+++ src/main/kotlin/com/dbmansfield/book/Args.kt
@@ -6,4 +6,8 @@ class Args(parser: ArgParser) {
     val library by parser.storing("-l", "--library", help = "").default("library")
     val service by parser.flagging("--gapplication-service")
     val terms by parser.positionalList("COMMAND", 1..Int.MAX_VALUE, help = "")
+
+    init {
+        parser.force()
+    }
 }
--- src/main/kotlin/com/dbmansfield/book/Library.kt
+++ src/main/kotlin/com/dbmansfield/book/Library.kt
@@ -5,17 +5,44 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import java.io.File
+import java.io.FileNotFoundException
 
-class Library(val file: File) {
+class Library(file: File) {
+    val books: Array<Book>
+
     init {
         if (file.canRead()) {
             val mapper = ObjectMapper(YAMLFactory())
                     .registerModule(KotlinModule())
                     .registerModule(JavaTimeModule())
-            val books: Array<Book> = mapper.readValue(file, Array<Book>::class.java)
-            println(books.first().asBibtex())
+            books = mapper.readValue(file, Array<Book>::class.java) ?: emptyArray()
         } else {
-            println("File not found")
+            throw FileNotFoundException("Cannot read $file")
+        }
+    }
+
+    companion object {
+        fun findLibrary(filename: String): Library {
+            // Load the library file, trying a few alternatives
+            val attempts = listOf(filename, "$filename.yml", "$filename.yaml").map(::File)
+            var f: File? = attempts.firstOrNull(File::canRead)
+            if (f == null) {
+                // None of the alternatives exist and are readable, so find the first
+                // one that doesn't exist
+                f = attempts.firstOrNull { !it.exists() }
+                if (f == null) {
+                    // All alternatives exist and cannot be read, so bail
+                    throw FileAlreadyExistsException(attempts.first(),
+                            reason = "Library already exists and is not readable")
+                }
+                if (!f.createNewFile()) {
+                    // An alternative is available but cannot be created, so bail
+                    throw FileNotFoundException("Cannot read library and failed to create alternative $f")
+                }
+                // Make the newly created file a valid empty YAML file
+                f.appendText("---\n")
+            }
+            return Library(f)
         }
     }
 }
